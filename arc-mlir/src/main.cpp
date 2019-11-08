5bfefcb896cc833d3c8a1eb2a1b06782a5bedaaa
Frej Drejhammar <frej@sics.se>
2019-11-08 11:28:14 +0100

    arc-mlir: Parse and dump mlir input
    Add just enough code from the tutorial to verify that we can link and
    run the MLIR libraries.

--- arc-mlir/src/main.cpp
+++ arc-mlir/src/main.cpp
@@ -1,7 +1,46 @@
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/ErrorOr.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/SourceMgr.h>
 #include <llvm/Support/raw_ostream.h>
+#include <memory>
+#include <mlir/Analysis/Verifier.h>
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/IR/Module.h>
+#include <mlir/Parser.h>
+
+namespace cl = llvm::cl;
+
+static cl::opt<std::string> inputFilename(cl::Positional,
+                                          cl::desc("<input arc file>"),
+                                          cl::init("-"),
+                                          cl::value_desc("filename"));
+
+static cl::opt<std::string> outputFilename(cl::Positional,
+                                           cl::desc("<output mlir file>"),
+                                           cl::init("-"),
+                                           cl::value_desc("filename"));
 
 int main(int argc, char *argv[]) {
+  cl::ParseCommandLineOptions(argc, argv, "arc-mlir tool\n");
 
-  llvm::errs() << "Hello world\n";
+  llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> fileOrErr =
+      llvm::MemoryBuffer::getFileOrSTDIN(inputFilename);
+  if (std::error_code EC = fileOrErr.getError()) {
+    llvm::errs() << "Could not open input file: " << EC.message() << "\n";
+    return -1;
+  }
+
+  mlir::MLIRContext context;
+  llvm::SourceMgr sourceMgr;
+  sourceMgr.AddNewSourceBuffer(std::move(*fileOrErr), llvm::SMLoc());
+  mlir::OwningModuleRef module = mlir::parseSourceFile(sourceMgr, &context);
+  if (!module) {
+    llvm::errs() << "Error can't load file " << inputFilename << "\n";
+    return 3;
+  }
+
+  module->dump();
+
   return 0;
 }
