cf6063722635420769efac898ce746f88580f376
Frej Drejhammar <frej@sics.se>
2019-11-12 12:10:37 +0100

    Run scalafmt on arc/Driver.scala NFC

--- src/main/scala/se/kth/cda/arc/Driver.scala
+++ src/main/scala/se/kth/cda/arc/Driver.scala
@@ -7,12 +7,13 @@ import se.kth.cda.arc.ast.printer.Printer._
 import scala.util.{Failure, Success}
 
 object Driver {
+
   def main(args: Array[String]): Unit = {
     args match {
-        // REPL
+      // REPL
       case Array("-r") =>
         RPPL.run()
-        // Read input file, write to output file
+      // Read input file, write to output file
       case Array("-f", format, "-i", inputPath, "-o", outputPath) =>
         val inputFile = scala.io.Source.fromFile(inputPath)
         val code = inputFile.getLines.mkString("\n")
@@ -22,11 +23,11 @@ object Driver {
         val bw = new BufferedWriter(new FileWriter(outputFile))
         bw.write(ast.toStringFormat(format))
         bw.close()
-        // Read string from command line, write to stdout
+      // Read string from command line, write to stdout
       case Array("-f", format, code) =>
         val ast = Compiler.run(code)
         Console.out.println(ast.toStringFormat(format))
-        // Read from stdin, write to stdout
+      // Read from stdin, write to stdout
       case Array("-f", format) =>
         val code = Console.in.readLine()
         val ast = Compiler.run(code)
