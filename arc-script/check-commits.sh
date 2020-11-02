c43b9b0f61322a6e3bc98bd446fa34f807a3ba92
Klas Segeljakt <klasseg@kth.se>
2020-11-03 00:42:20 +0100

    cd back to original directory after checking commit

--- arc-script/check-commits.sh
+++ arc-script/check-commits.sh
@@ -3,13 +3,13 @@
 if [ $# -eq 1 ]
 then
   git rebase "$1" \
-    --exec="cd arc-script && cargo check --all-features --bins --examples --tests --benches" \
-    --exec="cd arc-script && cargo test" \
-    --exec="cd arc-script && cargo clippy" \
-    --exec="cd arc-script && cargo fmt -- --check" \
-    --exec="cd arc-script/core && cargo rustc --lib -- -D warnings" \
-    --exec="cd arc-script/wasm && cargo rustc --lib -- -D warnings" \
-    --exec="cd arc-script/macros && cargo rustc --lib -- -D warnings"
+    --exec="cd arc-script; cargo check --all-features --bins --examples --tests --benches; cd -" \
+    --exec="cd arc-script; cargo test; cd -" \
+    --exec="cd arc-script; cargo clippy; cd -" \
+    --exec="cd arc-script; cargo fmt -- --check; cd -" \
+    --exec="cd arc-script/core && cargo rustc --lib -- -D warnings; cd -" \
+    --exec="cd arc-script/wasm && cargo rustc --lib -- -D warnings; cd -" \
+    --exec="cd arc-script/macros && cargo rustc --lib -- -D warnings; cd -"
 else
   echo "Expected branch as argument."
 fi
