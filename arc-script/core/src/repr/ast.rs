660a5f9b7bfedd2c820263e2a13274139c56fee6
Klas Segeljakt <klasseg@kth.se>
2020-11-04 18:21:44 +0100

    Remove outdated comment

--- arc-script/core/src/repr/ast.rs
+++ arc-script/core/src/repr/ast.rs
@@ -60,42 +60,6 @@ pub struct TypeDef {
 }
 
 /// A task is a generic low-level primitive which resembles a node in the dataflow graph.
-///
-/// Tasks may be specialized into any higher order function (e.g., map, filter) by implementing
-/// three callback functions:
-/// * setup():             invoked when the operator is created (optional).
-/// * process(self, elem): invoked on each arriving data stream element (required).
-/// * trigger(self):       invoked when a timer is triggered (optional).
-///
-/// In addition, the operator itself has builtin methods for controlling its functionality:
-/// * self.send(<channel>, <data>):   sends <data> over <channel>
-/// * self.schedule(<time>):          schedules a timer to trigger at a given <time>
-/// * self.update(<field> = <state>): updates <field> to contain a new <state>
-///
-/// ```text
-/// task map(input, fun) -> (output)
-///     fun process(self, elem) = self.output(self.fun(elem))
-/// end
-///
-/// task filter(input, fun) -> (output)
-///     fun process(self, elem) =
-///         if fun(elem)
-///         then self.send(output, elem)
-///         else self
-/// end
-///
-/// task flat_map(input, fun) -> (output)
-///     fun process(self, elem) =
-///         fun(elem).fold(self, (self, elem) => self.send(output, elem))
-/// end
-///
-/// task partition(input, fun) -> (output1, output2)
-///     fun process(self, elem) =
-///         if fun(elem)
-///         then self.send(output1, elem)
-///         else self.send(output2, elem)
-/// end
-/// ```
 #[derive(Constructor)]
 pub struct TaskDef {
     pub id: Ident,
