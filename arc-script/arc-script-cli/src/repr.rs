6d0ac3fd26834f8fd86855c92d94f35006717c73
Klas Segeljakt <klasseg@kth.se>
2021-02-01 11:40:05 +0100

    Change Opt into something more useful

--- arc-script/arc-script-cli/src/repr.rs
+++ arc-script/arc-script-cli/src/repr.rs
@@ -5,6 +5,7 @@ use derive_more::From;
 use std::path::PathBuf;
 
 pub use clap::{ArgEnum, Clap};
+use strum::EnumString;
 
 /// Specification of the Arc-script command-line interface (CLI).
 #[derive(Clap, Debug, Clone, Default)]
@@ -46,19 +47,6 @@ pub enum SubCmd {
     /// Compile and execute source file
     #[clap(name = "run")]
     Run(Run),
-
-    /// Compile and test source file.
-    /// NB: For now, this just generates Rust.
-    #[clap(name = "test")]
-    Test(Test),
-
-    /// Compile and check source file
-    #[clap(name = "check")]
-    Check(Check),
-
-    /// Only parse source file
-    #[clap(name = "parse")]
-    Parse(Parse),
 }
 
 impl Default for SubCmd {
@@ -73,27 +61,22 @@ pub struct Run {
     /// Path to main file
     #[clap(parse(from_os_str))]
     pub main: Option<PathBuf>,
+    /// Output mode: [AST|HIR|DFG|Rust|MLIR]
+    #[clap(long)]
+    pub output: Output,
 }
 
-#[derive(Clap, Default, Debug, Clone, New)]
-pub struct Test {
-    /// Path to main file
-    #[clap(parse(from_os_str))]
-    pub main: Option<PathBuf>,
+#[derive(ArgEnum, Debug, Clone, EnumString)]
+pub enum Output {
+    AST,
+    HIR,
+    DFG,
+    Rust,
+    MLIR,
 }
 
-/// Configuration parameters for the `check` subcommand.
-#[derive(Clap, Default, Debug, Clone, New)]
-pub struct Check {
-    /// Path to main file
-    #[clap(parse(from_os_str))]
-    pub main: Option<PathBuf>,
-}
-
-/// Configuration parameters for the `parse` subcommand.
-#[derive(Clap, Default, Debug, Clone, New)]
-pub struct Parse {
-    /// Path to main file
-    #[clap(parse(from_os_str))]
-    pub main: Option<PathBuf>,
+impl Default for Output {
+    fn default() -> Self {
+        Self::MLIR
+    }
 }
