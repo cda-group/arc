cb5dfec8ac9d3615bd86ba678776f17732f70e96
Klas Segeljakt <klasseg@kth.se>
2021-02-19 00:02:11 +0100

    Split arc-script-test-integration into subcrates

--- arc-script/arc-script-test/integration/dynamic-cached/src/main.arc
+++ arc-script/arc-script-test/integration/dynamic-cached/src/main.arc
@@ -1,3 +1,7 @@
+fun pipe(s: ~i32) -> ~i32 {
+    Identity() (s)
+}
+
 task Identity() (i32) -> (i32) {
     on event => emit event
 }
--- arc-script/arc-script-test/integration/dynamic-cached/src/main.rs
+++ arc-script/arc-script-test/integration/dynamic-cached/src/main.rs
@@ -1,10 +1,6 @@
 use arcon::prelude::ArconTime;
 use arcon::prelude::Pipeline;
 
-/// Directly compiles and includes an Arc-program.
-#[arc_script::compile("script.arc")]
-mod script {}
-
 /// Includes a pre-built Arc-program
 #[arc_script::include("main.arc")]
 mod main {}
@@ -17,11 +13,10 @@ fn main() {
         conf.set_arcon_time(ArconTime::Process);
     });
 
-    let stream1 = script::pipe(stream0);
+    let stream1 = main::pipe(stream0);
 
     let mut pipeline = stream1.to_console().build();
 
     pipeline.start();
     pipeline.await_termination();
 }
-
