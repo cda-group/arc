3b185d56ed6aadba51996de20c0f5366b3f5a479
Klas Segeljakt <klasseg@kth.se>
2021-02-20 18:27:43 +0100

    Add #[arcorn::rewrite] proc macro for rewriting enums
    This adds a clever proc macro which can be used like:
    
    ```
    enum Foo {
        Bar(i32),
        Baz(Qux)
    }
    
    struct Qux {
        x: i32,
        y: i32
    }
    ```
    
    It will rewrite the items into a format which is compatible with Arcon
    and Protobuf (prost). The transformed enums can afterwards be accessed
    through the `arcorn::{is, unwrap, enwrap}` macros. See their doc-tests for
    more documentation.

--- arc-script/arc-script-api/arcorn/src/enums.rs
+++ arc-script/arc-script-api/arcorn/src/enums.rs
@@ -7,53 +7,13 @@
 //!
 //! All of these assumptions will be ensured by the Arcorn interface.
 
-/// Declares a new enum which is compatible with the `arcorn::{enwrap, unwrap, is}` API.
-///
-/// Any expansion of the macro satisfies the following properties:
-/// * Each enum is wrapped inside a struct (prost requirement).
-/// * Each enum implements a method `.wrap()` to wrap it inside the struct.
-/// * Each enum variants is imported into the global namespace.
-///
-/// ```
-/// arcorn::declare_enum! {
-///     enum Foo {
-///         FooBar(i32),
-///         FooBaz(i32)
-///     }
-/// }
-/// ```
-#[macro_export]
-macro_rules! declare_enum {
-    {
-        enum $name:ident {
-            $($variant:ident($ty:ty)),*
-        }
-    } => {
-        paste::paste! {
-            pub struct $name {
-                this: [<$name Enum>]
-            }
-            pub enum [<$name Enum>] {
-                $($variant($ty)),*
-            }
-            use [<$name Enum>]::*;
-            impl [<$name Enum>] {
-                fn wrap(self) -> $name {
-                    $name { this: self }
-                }
-            }
-        }
-    }
-}
-
 /// Enwraps a value into an enum-variant.
 ///
 /// ```
-/// arcorn::declare_enum! {
-///     enum Foo {
-///         FooBar(i32),
-///         FooBaz(i32)
-///     }
+/// #[arcorn::rewrite]
+/// enum Foo {
+///     FooBar(i32),
+///     FooBaz(i32)
 /// }
 /// let foo = arcorn::enwrap!(FooBar, 5);
 /// ```
@@ -69,11 +29,10 @@ macro_rules! enwrap {
 /// Returns `true` if enum is a certain variant, else `false`.
 ///
 /// ```
-/// arcorn::declare_enum! {
-///     enum Foo {
-///         FooBar(i32),
-///         FooBaz(i32)
-///     }
+/// #[arcorn::rewrite]
+/// enum Foo {
+///     FooBar(i32),
+///     FooBaz(i32)
 /// }
 ///
 /// let foo = arcorn::enwrap!(FooBar, 5);
@@ -84,7 +43,7 @@ macro_rules! is {
     {
         $variant:ident , $expr:expr
     } => {
-        if let $variant(_) = &$expr.this {
+        if let Some($variant(_)) = &$expr.this {
             true
         } else {
             false
@@ -95,11 +54,10 @@ macro_rules! is {
 /// Unwraps a value out of an enum-variant.
 ///
 /// ```
-/// arcorn::declare_enum! {
-///     enum FooEnum {
-///         FooBar(i32),
-///         FooBaz(i32)
-///     }
+/// #[arcorn::rewrite]
+/// enum FooEnum {
+///     FooBar(i32),
+///     FooBaz(i32)
 /// }
 ///
 /// let foo = arcorn::enwrap!(FooBar, 5);
@@ -110,7 +68,7 @@ macro_rules! unwrap {
     {
         $variant:ident , $expr:expr
     } => {
-        if let $variant(v) = $expr.this {
+        if let Some($variant(v)) = $expr.this {
             v
         } else {
             unreachable!()
