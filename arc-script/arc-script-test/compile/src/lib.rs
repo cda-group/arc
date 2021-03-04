213d4e8f1db3800e7a3cef84485084e701aecadd
Klas Segeljakt <klasseg@kth.se>
2021-03-04 15:56:06 +0100

    Switch for the moment to doing compiletests at compiletime

--- arc-script/arc-script-test/compile/src/lib.rs
+++ arc-script/arc-script-test/compile/src/lib.rs
@@ -1,8 +1,40 @@
+#![allow(unused)]
+
 #[cfg(test)]
 mod insta;
 
 // #[cfg(test)]
 // mod trybuild;
 
-#[cfg(test)]
-mod compiletest;
+// #[cfg(test)]
+// mod compiletest;
+
+mod tests {
+    mod expect_pass {
+        mod basic_pipe;
+        mod binops;
+        mod enum_pattern;
+        mod enum_pattern_nested;
+        mod enums;
+        mod fib;
+        mod fun;
+        mod if_let;
+        mod ifs;
+        mod lambda;
+        mod literals;
+        mod nested_if;
+        mod option;
+        mod path;
+        mod pattern;
+        mod r#if;
+        mod structs;
+    }
+    mod expect_mlir_fail_todo {
+        mod extern_fun;
+        mod pipe;
+        mod task_filter;
+        mod task_identity_untagged;
+        mod task_map;
+        mod task_with_funs;
+    }
+}
