5a6aca91763135308f0fa26e4a5d7a247355157c
Klas Segeljakt <klasseg@kth.se>
2021-06-10 13:08:00 +0200

    WIP

--- mlir-dialect-experiments/exec-mlir-from-rust/build.rs
+++ mlir-dialect-experiments/exec-mlir-from-rust/build.rs
@@ -2,5 +2,5 @@ use std::process::Command;
 
 fn main() {
     Command::new("make").output().unwrap();
-    println!("cargo:rustc-link-search={}/mlir-target", env!("CARGO_MANIFEST_DIR"));
+    println!("cargo:rustc-link-search={}/mlir/target", env!("CARGO_MANIFEST_DIR"));
 }
