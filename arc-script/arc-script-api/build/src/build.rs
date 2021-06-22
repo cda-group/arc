0cdae93554a4cee1d58bc2bf960a3387ff290ec4
Klas Segeljakt <klasseg@kth.se>
2021-06-22 16:18:28 +0200

    Refactor arc-script-api/build
    Now it is possible to
    
    * Prefix all generated artifacts with a string
    * Force optimisations to be enabled when building (using MLIR)

--- arc-script/arc-script-api/build/src/build.rs
+++ arc-script/arc-script-api/build/src/build.rs
@@ -35,38 +35,29 @@ impl Builder {
         let out_dir = &env::var("OUT_DIR").unwrap();
 
         if self.source_dirs.is_empty() {
-            self.source_dirs.push("".to_owned());
+            self.source_dirs.push(String::new());
         }
+
+        let msg = Arc::new(Mutex::new(String::new()));
+        set_hook(msg.clone());
 
         for source_dir in &self.source_dirs {
             let source_dir = Path::new(cargo_dir).join(source_dir);
             for entry in WalkDir::new(source_dir).into_iter().filter_map(Result::ok) {
                 let mut path = entry.path();
                 if let Some(name) = path.file_name() {
-                    if name == "main.arc" || has_extension(path, "arc") && self.no_exclude {
-                        let rc0 = Arc::new(Mutex::new(String::from("hello")));
-                        let rc1 = rc0.clone();
-
-                        std::panic::set_hook(Box::new(move |panic_info| {
-                            let mut payload = rc1.lock().unwrap();
-
-                            let loc = if let Some(location) = panic_info.location() {
-                                format!(" in '{}' at line {}", location.file(), location.line(),)
-                            } else {
-                                String::from("")
-                            };
-                            let msg = if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
-                                format!("'{}'", s)
-                            } else {
-                                String::from("")
-                            };
-                            *payload = format!("{}{}", msg, loc);
-                        }));
-
+                    if name == "main.arc"
+                        || has_extension(path, "arc") && self.compile_unused_sources
+                    {
                         let res = panic::catch_unwind(|| self.build_file(path, out_dir, cargo_dir));
                         if let Err(e) = res {
-                            let message = rc0.lock().unwrap();
-                            self.report_arcscript_error(path, &message, out_dir, cargo_dir);
+                            let mut file =
+                                self.create_output_file(PathBuf::from(path), cargo_dir, out_dir);
+                            writeln!(
+                                file,
+                                r#"compile_error!("Internal Compiler Error: {}");"#,
+                                msg.lock().unwrap()
+                            );
                         }
                     }
                 }
@@ -81,35 +72,31 @@ impl Builder {
 
         // Compile file
         let mut sink = Buffer::no_color();
-        let mode = Mode {
+        let mut mode = Mode {
             input: Input::File(Some(input_path.clone())),
-            output: get_rust_backend(),
+            output: if self.enable_optimisations {
+                Output::RustMLIR
+            } else {
+                get_rust_backend()
+            },
             ..Default::default()
         };
 
+        let mut file = self.create_output_file(input_path, cargo_dir, out_dir);
         match compile(mode, &mut sink) {
             Err(_) => {
-                eprintln!(
-                    "Internal compiler error when compiling : {:?}",
-                    input_path.into_os_string()
+                writeln!(
+                    file,
+                    r#"compile_error!("Internal Compiler Error: {}");"#,
+                    std::str::from_utf8(sink.as_slice()).unwrap()
                 );
-                panic!("{}", std::str::from_utf8(sink.as_slice()).unwrap())
             }
             Ok(report) => {
-                // Path to /a/b/c/my-project/target/build/my-project-xxx/out/src/x/y/z/main.rs
-                let mut output_path = input_path;
-                output_path.set_extension("rs");
-                let output_path = output_path.strip_prefix(cargo_dir).unwrap();
-                let output_path = PathBuf::from(out_dir).join(output_path);
-
-                let output_dir = output_path.parent().unwrap();
-                fs::create_dir_all(output_dir).unwrap();
-                let mut file = fs::File::create(output_path).unwrap();
-
                 if report.is_ok() {
                     file.write_all(sink.as_slice()).unwrap();
                 } else {
-                    file.write_all(r##"compile_error!(r#"Compilation Failed: "##.as_bytes()).unwrap();
+                    file.write_all(r##"compile_error!(r#"Compilation Failed: "##.as_bytes())
+                        .unwrap();
                     file.write_all(sink.as_slice()).unwrap();
                     file.write_all(r##""#);"##.as_bytes()).unwrap();
                 }
@@ -117,26 +104,38 @@ impl Builder {
         }
     }
 
-    // Write a fake output file, which when included into the main
-    // module will report the compilation error.
-    fn report_arcscript_error(&self, path: &Path, message: &str, out_dir: &str, cargo_dir: &str) {
-        let input_path = PathBuf::from(path);
-        let mut output_path = input_path;
-        output_path.set_extension("rs");
-        let output_path = output_path.strip_prefix(cargo_dir).unwrap();
+    fn create_output_file(
+        &self,
+        mut path: PathBuf,
+        cargo_dir: &str,
+        out_dir: &str,
+    ) -> std::fs::File {
+        if let Some(prefix) = &self.prefix {
+            let mut filename = path.file_name().unwrap().to_str().unwrap().to_owned();
+            path.set_file_name(format!("{}{}", prefix, filename))
+        }
+        path.set_extension("rs");
+        let output_path = path.strip_prefix(cargo_dir).unwrap();
         let output_path = PathBuf::from(out_dir).join(output_path);
-
         let output_dir = output_path.parent().unwrap();
         fs::create_dir_all(output_dir).unwrap();
-        let mut sink = Buffer::no_color();
-        writeln!(
-            sink,
-            r#"compile_error!("Internal Compiler Error: {}");"#,
-            message
-        );
-        fs::File::create(output_path)
-            .unwrap()
-            .write_all(sink.as_slice())
-            .unwrap();
+        fs::File::create(output_path).unwrap()
     }
+}
+
+fn set_hook(msg: Arc<Mutex<String>>) {
+    std::panic::set_hook(Box::new(move |panic_info| {
+        let location = panic_info
+            .location()
+            .map(|location| format!(" in '{}' at line {}", location.file(), location.line()))
+            .unwrap_or_else(String::new);
+
+        let payload = panic_info
+            .payload()
+            .downcast_ref::<&str>()
+            .map(|s| format!("'{}'", s))
+            .unwrap_or_else(String::new);
+
+        *msg.lock().unwrap() = format!("{}{}", payload, location);
+    }))
 }
--- arc-script/arc-script-api/build/src/lib.rs
+++ arc-script/arc-script-api/build/src/lib.rs
@@ -6,14 +6,33 @@ mod build;
 
 #[derive(Default)]
 pub struct Builder {
-    no_exclude: bool,
+    compile_unused_sources: bool,
     source_dirs: Vec<String>,
+    prefix: Option<String>,
+    enable_optimisations: bool,
 }
 
 impl Builder {
     /// Compiles all scripts, regardless of whether they are used or not.
-    pub fn no_exclude(self, no_exclude: bool) -> Self {
-        Self { no_exclude, ..self }
+    pub fn compile_unused_sources(self, compile_unused_sources: bool) -> Self {
+        Self {
+            compile_unused_sources,
+            ..self
+        }
+    }
+    /// Compiles all scripts, regardless of whether they are used or not.
+    pub fn enable_optimisations(self, enable_optimisations: bool) -> Self {
+        Self {
+            enable_optimisations,
+            ..self
+        }
+    }
+    /// Prefixes output filename with
+    pub fn prefix_output_filename_with(self, prefix: impl Into<String>) -> Self {
+        Self {
+            prefix: Some(prefix.into()),
+            ..self
+        }
     }
     /// Start compiling from these directories. By default, compilation starts from the
     /// build-script's directory.
