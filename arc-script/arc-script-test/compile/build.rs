bd84e405037624544ba30bf470005008ec2e500b
Klas Segeljakt <klasseg@kth.se>
2021-06-30 15:05:25 +0200

    Test both Arc-Script and Arc-MLIR when running compile-tests

--- arc-script/arc-script-test/compile/build.rs
+++ arc-script/arc-script-test/compile/build.rs
@@ -4,4 +4,11 @@ fn main() {
         .enable_optimisations(false)
         .source_dirs(["src/tests/expect_pass", "src/tests/expect_mlir_fail_todo"])
         .build();
+
+    arc_script_build::Builder::default()
+        .compile_unused_sources(true)
+        .enable_optimisations(true)
+        .prefix_output_filename_with("opt_")
+        .source_dirs(["src/tests/expect_pass"])
+        .build();
 }
